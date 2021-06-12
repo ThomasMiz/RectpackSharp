5215fa6b7b7c33abc3ad6c8f5709c971604aa4d3
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2021-06-12 14:42:44 -0300

    Changed lock to use separate object for better thread safety

--- RectpackSharp/RectanglePacker.cs
+++ RectpackSharp/RectanglePacker.cs
@@ -10,6 +10,7 @@ namespace RectpackSharp
     {
         /// <summary>A weak reference to the last list used, so it can be reused in subsequent packs.</summary>
         private static WeakReference<List<PackingRectangle>> oldListReference;
+        private static readonly object oldListReferenceLock = new object();
 
         /// <summary>
         /// Finds a way to pack all the given rectangles into a single bin. Performance can be traded for
@@ -262,7 +263,7 @@ namespace RectpackSharp
             if (oldListReference == null)
                 return new List<PackingRectangle>(preferredCapacity);
 
-            lock (oldListReference)
+            lock (oldListReferenceLock)
             {
                 if (oldListReference.TryGetTarget(out List<PackingRectangle> list))
                 {
@@ -284,7 +285,7 @@ namespace RectpackSharp
                 oldListReference = new WeakReference<List<PackingRectangle>>(list);
             else
             {
-                lock (oldListReference)
+                lock (oldListReferenceLock)
                 {
                     if (!oldListReference.TryGetTarget(out List<PackingRectangle> oldList) || oldList.Capacity < list.Capacity)
                         oldListReference.SetTarget(list);
